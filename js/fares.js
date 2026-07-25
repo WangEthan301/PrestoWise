3e7677b1cb2e0cae3e078e5d66e2804bf9718be3
CheesyintheHead <149900036+CheesyintheHead@users.noreply.github.com>
2026-07-25 10:49:01 -0400

    feat: filled fare object

---
+++ js/fares.js
@@ -0,0 +1,54 @@
+export const fares = {
+    transferWindow: 120,
+    "GO Transit": {
+        newTransferWindow: 180
+    },
+    "Miway": {
+        Adult: 3.50,
+        Senior: 0.00,
+        Youth: 2.90,
+        Child: 0.00
+    },
+    "TTC": {
+        Adult: 3.30,
+        Senior: 2.25,
+        Youth: 2.35,
+        Child: 0.00
+    },
+    "York Region Transit": {
+        Adult: 4.24,
+        Senior: 2.60,
+        Youth: 3.29,
+        Child: 2.60
+    },
+    "Oakville Transit": {
+        Adult: 3.50,
+        Senior: 0.00,
+        Youth: 0.00,
+        Child: 0.00
+    },
+    "Burlington Transit": {
+        Adult: 2.85,
+        Senior: 0.00,
+        Youth: 2.00,
+        Child: 0.00
+    },
+    "Hamilton Street Railway": {
+        Adult: 2.85,
+        Senior: 2.35,
+        Youth: 2.35,
+        Child: 0.00
+    },
+    "Brampton Transit": {
+        Adult: 3.55,
+        Senior: 1.85,
+        Youth: 2.95,
+        Child: 2.00
+    },
+    "Durham Region Transit": {
+        Adult: 3.84,
+        Senior: 2.53,
+        Youth: 3.46,
+        Child: 0.00
+    }
+}
